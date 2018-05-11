d66c09815055e0e08850a0f5cb6b31f7b25a8787
Bharat Kunwar <b.kunwar@gmail.com>
2018-05-11 14:04:07 +0100

    Review changes

--- library/os_container_infra.py
+++ library/os_container_infra.py
@@ -15,78 +15,76 @@
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.utils.display import Display
-from magnumclient.client import Client
+from magnumclient.client import Client as MagnumClient
 from magnumclient.exceptions import *
+from heatclient.client import Client as HeatClient
 import openstack
 import time
+import json
 
 display = Display()
 
-def get_os_cloud(cloud_name, module):
+class WaitCondition(Exception):
+    def __init__(self, message, errors):
+
+        # Call the base class constructor with the parameters it needs
+        super(WaitCondition, self).__init__(message)
+
+        # Now for your custom code...
+        self.errors = errors
+
+def get_os_cloud(cloud_name):
     os_cloud = openstack.connect(cloud=cloud_name)
     os_cloud.authorize()
     return os_cloud
 
-def get_update_or_create_container_infra(magnum_client, module, **kwargs):
+def get_container_infra(magnum_client, create_if_not_found=True, **kwargs):
     try:
-        # Get cluster details
-        container_infra_details = magnum_client.clusters.get(kwargs['name'])
-        if container_infra_details.status.endswith('FAILED'):
-            # If the cluster is in a failed status, raise error:
-            module.fail_json(
-                msg="Cluster is not usable because: {}.".format(container_infra_details.faults))
-        elif container_infra_details.status.endswith('PROGRESS'):
-            # If the cluster creation and update is in progress:
-            display.warning(container_infra_details.status)
-            return False
-        elif container_infra_details.status.endswith('COMPLETE'):
-            # If the cluster looks ready
-            # Raise error if there is a mismatch between cluster template ids
-            cluster_template_mismatch = kwargs['cluster_template_id'] != container_infra_details.cluster_template_id 
-            if cluster_template_mismatch:
-                module.fail_json(
-                    msg="Cluster template mismatch for an existing cluster.")
-
-            patch_list = list()
-
-            # Update number of masters if there is a mismatch
-            master_count_mismatch = kwargs['master_count'] != container_infra_details.master_count
-            if master_count_mismatch:
-                patch = dict(op='replace',
-                     value=kwargs['master_count'],
-                     path='/master_count')
-                patch_list.append(patch)
-
-            # Update number of nodes if there is a mismatch
-            node_count_mismatch = kwargs['node_count'] != container_infra_details.node_count
-            if node_count_mismatch:
-                patch = dict(op='replace',
-                     value=kwargs['node_count'],
-                     path='/node_count')
-                patch_list.append(patch)
-
-            if patch_list:
-                # There appears to be a patch needed to apply
-                magnum_client.clusters.update(container_infra_details.uuid, patch_list)
-                display.warning("Applying patch to the cluster: {}".format(patch_list))
-                return False
-            else:
-                # Everything looks okay, return cluster details
-                return container_infra_details
-        else:
-            module.fail_json(
-                msg="Cluster returned unexpected status: {}".format(container_infra_details.status))
-    except NotFound:
+        return magnum_client.clusters.get(kwargs['name'])
+    except NotFound as e:
         # Create new cluster here if no cluster with the name was found
-        magnum_client.clusters.create(**kwargs)
-        display.warning("Cluster called {} does not exist, creating new.".format(kwargs['name']))
-        return False
+        if create_if_not_found:
+            magnum_client.clusters.create(**kwargs)
+            raise WaitCondition("Cluster called {} does not exist, creating new.".format(kwargs['name']))
+        else:
+            raise e
 
+def update_container_infra(magnum_client, container_infra, **kwargs):
+    # Raise module error if there is a mismatch between cluster template ids
+    cluster_template_mismatch = kwargs['cluster_template_id'] != container_infra.cluster_template_id 
+    if cluster_template_mismatch:
+        module.fail_json(
+            msg="Cluster template mismatch for an existing cluster.")
+
+    # Patch list to append list of patches
+    patch_list = list()
+
+    # Update number of masters if there is a mismatch
+    master_count_mismatch = kwargs['master_count'] != container_infra.master_count
+    if master_count_mismatch:
+        patch = dict(op='replace',
+             value=kwargs['master_count'],
+             path='/master_count')
+        patch_list.append(patch)
+
+    # Update number of nodes if there is a mismatch
+    node_count_mismatch = kwargs['node_count'] != container_infra.node_count
+    if node_count_mismatch:
+        patch = dict(op='replace',
+             value=kwargs['node_count'],
+             path='/node_count')
+        patch_list.append(patch)
+
+    if len(patch_list) > 0:
+        # There appears to be a patch needed to apply
+        magnum_client.clusters.update(container_infra.uuid, patch_list)
+        raise WaitCondition("Applying patch to the cluster: {}".format(patch_list))
+        
 def main():
     module = AnsibleModule(
         argument_spec = dict(
             cloud_name=dict(required=True, type='str'),
-            magnum_endpoint=dict(required=True, type='str'),
+            create_if_not_found=dict(required=True, type='bool'),
             cluster_name=dict(required=True, type='str'),
             cluster_template_name=dict(required=True, type='str'),
             master_count=dict(required=True, type='int'),
@@ -96,10 +94,8 @@ def main():
         supports_check_mode=False
     )
 
-    cloud_name = module.params['cloud_name']
-    magnum_endpoint = module.params['magnum_endpoint']
-    os_cloud = get_os_cloud(cloud_name,  module)
-    magnum_client = Client('1', endpoint_override=magnum_endpoint, session=os_cloud.session)
+    os_cloud = get_os_cloud(module.params['cloud_name'])
+    magnum_client = MagnumClient('1', endpoint_override=False, session=os_cloud.session)
 
     cluster_template_name = module.params['cluster_template_name']
     cluster_template_id = magnum_client.cluster_templates.get(cluster_template_name).uuid
@@ -114,23 +110,46 @@ def main():
     tries = 0
     fail_after = 100
     changed = False
-    while True:
-        container_infra_details = get_update_or_create_container_infra(magnum_client, module, **kwargs)
-        display.warning(container_infra_details)
-        if container_infra_details:
-            stack_id = container_infra_details.stack_id
-            display.warning(stack_id)
-            module.exit_json(changed=changed, details=stack_id)
-        else:
-            # Wait until this is not False
+    container_infra = False
+    while container_infra == False:
+        try:
+            # Get cluster info or raise WaitCondition if a new one is created.
+            container_infra = get_container_infra(magnum_client, module.params['create_if_not_found'], **kwargs)
+            if container_infra.status.endswith('FAILED'):
+                # If the cluster is in a failed status, raise error:
+                module.fail_json(
+                    msg="Cluster is not usable because: {}.".format(container_infra.faults))
+            elif container_infra.status.endswith('PROGRESS'):
+                # If the cluster creation and update is in progress:
+                raise WaitCondition(container_infra.status)
+            elif container_infra.status.endswith('COMPLETE'):
+                # Since the cluster looks ready, raise a WaitCondition if cluster is updated.
+                update_container_infra(magnum_client, container_infra, **kwargs)
+            else:
+                module.fail_json(
+                    msg="Cluster returned unexpected status: {}".format(container_infra.status))
+        except WaitCondition as e:
             if fail_after < tries:
+                # This is taking far too long, terminate.
                 module.fail_json(
                     msg="Failing after {} tries.".format(fail_after))
             else:
-                display.warning('Tries: [{}/{}]'.format(tries, fail_after))
+                # Wait before trying again.
+                display.debug('[DEBUG] Tries: [{}/{}] {}'.format(tries, fail_after, e))
                 time.sleep(10)
                 tries += 1
                 changed = True
+                container_infra = False
+
+    stack_id = container_infra.stack_id
+    display.debug('[DEBUG] Value of stack_id: {}'.format(stack_id))
+
+    stack_resources = get_stack_resources(stack_id)
+
+    heat_client = HeatClient('1', session=os_cloud.session)
+    resources = heat_client.resources.list(stack_id,nested_depth=4)
+
+    module.exit_json(changed=changed, container_infra=container_infra.to_dict())
 
 if __name__ == '__main__':
     main()
